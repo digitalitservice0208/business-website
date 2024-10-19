f5f700ae6c97278acf5bbbc4da5f44c5e377c4e8
apollo <apollo@email.com>
2024-10-19 12:20:03 +0200

    chore: added gh pages

--- src/pages/ourTeam.jsx
+++ src/pages/ourTeam.jsx
@@ -53,8 +53,10 @@ const OurTeam = () => {
                                                 <div className="ct-heading-hexagon"></div>
                                               </div>
                                               <div className="ct-heading-text-below"> Team</div>
-                                              <h3 className="item--title case-animate-time st-hexagon " data-wow-delay="ms">
-                                                <span>We provide our customers with the highest quality service</span></h3>
+                                              <h3 className="item--title case-animate-time st-hexagon "
+                                                  data-wow-delay="ms">
+                                                <span>We provide our customers with the highest quality service</span>
+                                              </h3>
                                             </div>
                                           </div>
                                         </div>
@@ -69,9 +71,11 @@ const OurTeam = () => {
                                           data-id="3b356ded" data-element_type="widget"
                                           data-widget_type="ct_text_editor.default">
                                           <div className="elementor-widget-container">
-                                            <div id="ct_text_editor-3b356ded" className="ct-text-editor " data-wow-delay="ms">
+                                            <div id="ct_text_editor-3b356ded" className="ct-text-editor "
+                                                 data-wow-delay="ms">
                                               <div className="ct-inline-css" data-css=""></div>
-                                              <div className="ct-text-editor elementor-clearfix"> We love what we do and we do
+                                              <div className="ct-text-editor elementor-clearfix"> We love what we do and
+                                                we do
                                                 it with passion. We value the experimentation of the message and smart
                                                 incentives.
                                               </div>
@@ -95,10 +99,8 @@ const OurTeam = () => {
                                                      className="no-lazyload "
                                                      src="/wp-content/uploads/2021/06/1.jpeg"
                                                      width="330" height="330" alt="team1"
-                                                     title="team1" /></a>
-                                              {/*<div className="item--social">*/}
-                                              {/*  <a href="#"><i className="fab fa-linkedin"></i></a>*/}
-                                              {/*</div>*/}
+                                                     title="team1" />
+                                              </a>
                                             </div>
                                             <div className="item--holder">
                                               <h4 className="item--title">Dedi Erianto Manullang</h4>
@@ -116,14 +118,9 @@ const OurTeam = () => {
                                                      width="330" height="330" alt="team2"
                                                      title="team2" />
                                               </a>
-                                              {/*<div className="item--social">*/}
-                                              {/*  <a href="#"><i className="fab fa-linkedin"></i></a>*/}
-                                              {/*</div>*/}
                                             </div>
                                             <div className="item--holder">
-                                              <h4 className="item--title">
-                                                <a href="#">Stefan Vukotic </a>
-                                              </h4>
+                                              <h4 className="item--title">Stefan Vukotic</h4>
                                               <div className="item--position">Head of Engineer</div>
                                             </div>
                                           </div>
@@ -137,14 +134,161 @@ const OurTeam = () => {
                                                      src="/wp-content/uploads/2021/06/3.png"
                                                      width="330" height="330" alt="team3"
                                                      title="team3" /></a>
-                                              {/*<div className="item--social">*/}
-                                              {/*  <a href="#"><i className="fab fa-linkedin"></i></a>*/}
-                                              {/*</div>*/}
                                             </div>
                                             <div className="item--holder">
                                               <h4 className="item--title">Sridahwany Sridahwany</h4>
                                               <div className="item--position">CPO</div>
+                                            </div>
+                                          </div>
+                                        </div>
+
+                                        <section
+                                          className="elementor-section elementor-top-section elementor-element elementor-element-3046ce3b elementor-section-full_width elementor-section-stretched elementor-section-height-default elementor-section-height-default ct-header-fixed-none ct-column-none ct-row-scroll-none ct-row-gradient--none"
+                                          data-id="3046ce3b" data-element_type="section">
+                                          <div className="elementor-container elementor-column-gap-no ">
+                                            <div
+                                              className="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-321d5fcc ct-column-none"
+                                              data-id="321d5fcc" data-element_type="column">
+                                              <div className="elementor-widget-wrap elementor-element-populated">
+                                                <div
+                                                  className="elementor-element elementor-element-509b890e elementor-widget-divider--view-line elementor-widget elementor-widget-divider"
+                                                  data-id="509b890e" data-element_type="widget"
+                                                  data-widget_type="divider.default">
+                                                  <div className="elementor-widget-container">
+                                                    <div className="elementor-divider"><span
+                                                      className="elementor-divider-separator"> </span></div>
+                                                  </div>
+                                                </div>
                                               </div>
+                                            </div>
+                                          </div>
+                                        </section>
+
+
+                                        <div className="grid-item col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+                                          <div className="item--inner item--inner1 wow fadeInUp"
+                                               data-wow-duration="1.2s">
+                                            <div className="item--holder item--holder1">
+                                              <div className="item--meta">
+                                                <h4 className="item--title item--title1">
+                                                  Toni Kukoc
+                                                </h4>
+                                                <div className="item--position item--position1">
+                                                  Front-End Developer
+                                                </div>
+                                              </div>
+                                            </div>
+                                            <div className="item--description">
+                                              I love crafting intuitive and visually appealing user interfaces. My
+                                              passion lies in creating seamless user experiences that delight visitors.
+                                              I enjoy staying up-to-date with the latest web design trends and
+                                              technologies.
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div className="grid-item col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+                                          <div className="item--inner item--inner1 wow fadeInUp"
+                                               data-wow-duration="1.2s">
+                                            <div className="item--holder item--holder1">
+                                              <div className="item--meta">
+                                                <h4 className="item--title item--title1">
+                                                  Luka Dzeko
+                                                </h4>
+                                                <div className="item--position item--position1">
+                                                  Back-End Developer
+                                                </div>
+                                              </div>
+                                            </div>
+                                            <div className="item--description">
+                                              Building the backbone of web applications is my specialty. I excel at
+                                              optimizing database performance, ensuring scalability, and implementing
+                                              robust security measures. My goal is to create efficient and reliable
+                                              back-end systems.
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div className="grid-item col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+                                          <div className="item--inner item--inner1 wow fadeInUp"
+                                               data-wow-duration="1.2s">
+                                            <div className="item--holder item--holder1">
+                                              <div className="item--meta">
+                                                <h4 className="item--title item--title1">
+                                                  Eduardo Quijando
+                                                </h4>
+                                                <div className="item--position item--position1">
+                                                  Full-Stack Developer
+                                                </div>
+                                              </div>
+                                            </div>
+                                            <div className="item--description">
+                                              I'm a versatile developer who can tackle both front-end and back-end
+                                              tasks. My ability to see the big picture and work across different layers
+                                              of an application is invaluable. I enjoy collaborating with developers
+                                              to create exceptional web experiences.
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div className="grid-item col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+                                          <div className="item--inner item--inner1 wow fadeInUp"
+                                               data-wow-duration="1.2s">
+                                            <div className="item--holder item--holder1">
+                                              <div className="item--meta">
+                                                <h4 className="item--title item--title1">
+                                                  Barys Sakovic
+                                                </h4>
+                                                <div className="item--position item--position1">
+                                                  JavaScript Developer
+                                                </div>
+                                              </div>
+                                            </div>
+                                            <div className="item--description">
+                                              JavaScript is my weapon of choice. I love exploring its capabilities
+                                              and creating dynamic and interactive web applications. My expertise in
+                                              JavaScript frameworks and libraries allows me to build efficient and
+                                              feature-rich websites and web applications.
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div className="grid-item col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+                                          <div className="item--inner item--inner1 wow fadeInUp"
+                                               data-wow-duration="1.2s">
+                                            <div className="item--holder item--holder1">
+                                              <div className="item--meta">
+                                                <h4 className="item--title item--title1">
+                                                  Bruno Petkovic
+                                                </h4>
+                                                <div className="item--position item--position1">
+                                                  UI / UX Developer
+                                                </div>
+                                              </div>
+                                            </div>
+                                            <div className="item--description">
+                                              Ensuring that websites look and function flawlessly on all devices is my
+                                              priority. I'm skilled in creating responsive designs that adapt to
+                                              different screen sizes and orientations. My goal is to provide a
+                                              consistent and enjoyable user experience across all platforms.
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div className="grid-item col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+                                          <div className="item--inner item--inner1 wow fadeInUp"
+                                               data-wow-duration="1.2s">
+                                            <div className="item--holder item--holder1">
+                                              <div className="item--meta">
+                                                <h4 className="item--title item--title1">
+                                                  Dejan Markovic
+                                                </h4>
+                                                <div className="item--position item--position1">
+                                                  Solution Architect
+                                                </div>
+                                              </div>
+                                            </div>
+                                            <div className="item--description">
+                                              Optimizing website speed and performance is a passion of mine. I use
+                                              various techniques to improve page load times, reduce resource usage, and
+                                              enhance overall user experience. My attention to detail and technical
+                                              expertise ensures that websites perform at their best.
+                                            </div>
                                           </div>
                                         </div>
                                       </div>
@@ -156,10 +300,14 @@ const OurTeam = () => {
                           </div>
                         </section>
                       </div>
-                    </div></article></main></div>
+                    </div>
+                  </article>
+                </main>
+              </div>
             </div>
           </div>
-        </div></div>
+        </div>
+      </div>
     </>
   );
 };
